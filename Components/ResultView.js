c9b1c4e5b52bb939813aa94faf232d8d856b3745
Keykon78 <67902056+Keykon78@users.noreply.github.com>
2022-01-09 15:09:57 +0100

    implemented design

--- Components/ResultView.js
+++ Components/ResultView.js
@@ -1,19 +1,42 @@
 import { ScanBtn } from "./ScanBtn"
 import {View, Text, StyleSheet } from "react-native"
 import React, { useRef } from "react";
+import { colors } from "../Assets/colors";
+import { hp, wp } from "../Helper/Converter";
+import { InfoBox } from "./InfoBox";
+import { HelpBtn } from "./HelpBtn";
+
 
 export const ResultView = ({title, mainView, btnText, onPress}) =>{
     console.log("In FailureVIew")
     return (
         <View style={styles.container}>
-            <Text>{title}</Text>
+            
+            <Text style={styles.header}>{title}</Text>
+            <InfoBox mainView={mainView} style={styles.infoBox}/>
             <ScanBtn text={btnText} onPress={onPress}/>
         </View>
     )
 }
 const styles = StyleSheet.create({
     container:{
-        flex:1
+        flex:1,
+        flexDirection:"column",
+        
+    },
+
+    header:{
+        marginBottom:hp(16),
+        marginTop: hp(208),
+        color: colors.fontWhite,
+        fontSize: 34,
+    },
+
+    infoBox:{
+        
+        marginTop: hp(200)
+        
+        
     }
 
 })
