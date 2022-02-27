a06003733442343568efc19b020107f94b4c9cc5
Keykon78 <67902056+Keykon78@users.noreply.github.com>
2022-02-27 21:25:06 +0100

    implemented substeps

--- Components/Solver/Components/Step.js
+++ Components/Solver/Components/Step.js
@@ -1,29 +1,64 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import print from '../print.js';
 import Change from '../Change.js';
-import { View } from 'react-native';
+import { TouchableOpacity, View, Text } from 'react-native';
 import mmlOptions from "../mmlOptions.js";
-import MathJax from 'react-native-mathjax';
+import MathJax from 'react-native-mathjax-svg';
+import { colors } from '../../../Assets/colors.js';
 
 export default class Step extends PureComponent {
 
+  constructor(props){
+    super(props)
+
+    this.state = {
+      expanded: false
+    }
+  }
 
   renderStep = (step) => {
-    return <View>
-      <MathJax mathJaxOptions={mmlOptions} html={"$"+print.newNode(step)+"$"}/>
+    var renderedSubsteps = <View></View>
+    
+    if(step.substeps){
+       renderedSubsteps = step.substeps.map(
+        (step, index) => <Step step={step} key={index}/>);
+    }
+    
+    const printMeth = step.step ? print.orgNode : print.newNode
+    const root = step.step ? <MathJax>{print.newNode(step.step)}</MathJax> : <View></View>
+  
+    return <View style={{marginTop: 10}}>
+      <MathJax>{printMeth(step)}</MathJax> 
+      
+      {step.substeps && 
+      <TouchableOpacity onPress={this.changeSubs} style={{backgroundColor: colors.primaryColor, marginVertical:5, width:40}}>
+        <Text>Sub</Text>
+      </TouchableOpacity> }
+
+      {this.state.expanded && 
+      <View style={{paddingLeft:15}}>
+        {root}
+        {renderedSubsteps}
+      </View>}
     </View>;
   }
 
+  changeSubs = () =>{
+    this.setState(
+      {expanded: !this.state.expanded}
+    )
+  }
 
   render() {
     const {step} = this.props;
 
+    const realStep = step.step ? step.step: step
   
     return(
-    <View>
-      <MathJax mathJaxOptions={mmlOptions} html={"$"+Change.formatChange(step)+"$"}/>
+    <View style={{marginTop:10}}>
+      <MathJax>{Change.formatChange(realStep)}</MathJax>
       {this.renderStep(step)}
     </View>
     )
